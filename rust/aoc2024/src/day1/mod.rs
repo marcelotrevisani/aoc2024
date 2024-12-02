37607f4df2683302b6be38c1ef9ead7b54094e78
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2024-12-02 10:52:47 +0000

    [Rust] Day 1, part 2

--- rust/aoc2024/src/day1/mod.rs
+++ rust/aoc2024/src/day1/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use regex::Regex;
 
@@ -9,6 +10,26 @@ pub fn part1() -> u32{
 
     let (mut left, mut right) = parse_input_day1(content.as_str());
     day1_part1(&mut left, &mut right)
+}
+
+pub fn part2() -> u32{
+    let current_file = file!();
+    let input1_path = std::path::Path::new(current_file).parent().unwrap().join("input1.txt");
+    let content = fs::read_to_string(input1_path).unwrap();
+
+    let (mut left, mut right) = parse_input_day1(content.as_str());
+    day1_part2(&mut left, &mut right)
+}
+
+pub fn day1_part2(left_list: &mut Vec<u32>, right_list: &mut Vec<u32>) -> u32 {
+    let mut table = HashMap::new();
+    
+    for key in right_list.iter() {
+        *table.entry(key).or_insert(0) += 1;
+    }
+    left_list.iter()
+        .map(|v| table.get(v).unwrap_or(&0) * v)
+        .sum()
 }
 
 pub fn parse_input_day1(input_txt: &str) -> (Vec<u32>, Vec<u32>) {
@@ -58,7 +79,21 @@ mod tests {
     }
 
     #[test]
+    fn test_day1_part2_simple(){
+        assert_eq!(day1_part2(
+            &mut vec![3, 4, 2, 1, 3, 3],
+            &mut vec![4, 3, 5, 3, 9, 3]),
+                   31
+        );
+    }
+
+    #[test]
     fn test_day1_part1_solution(){
         assert_eq!(part1(), 2031679);
+    }
+
+    #[test]
+    fn test_day1_part2_solution(){
+        assert_eq!(part2(), 19678534);
     }
 }
